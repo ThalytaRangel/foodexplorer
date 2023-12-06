f3205769b406d1bd1ef29a77392b7dbaf3759ec8
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-06 17:55:58 -0300

    styles/TASK-0052: make the components on page NewDish resposive

--- src/pages/NewDish/styles.js
+++ src/pages/NewDish/styles.js
@@ -1,9 +1,10 @@
 import styled from "styled-components";
-import { DEVICE_BREAKPOINT } from "../../styles/devicesBreakpoints";
+import { DEVICE_BREAKPOINT} from "../../styles/devicesBreakpoints"
 
 export const Container = styled.div`
-  width: 100vw;
+  width: 100%;
   height: 100vh; 
+ 
   
   display: grid;
   align-items: center;
@@ -12,41 +13,49 @@ export const Container = styled.div`
   grid-template-areas: 
   "header"
   "content"
-  "footer";  
+  "footer";
 `
 
 export const Content = styled.div`
   grid-area: content;
-  margin: auto;
   width: 100%;
-  height: 100%;
+  max-width: 112rem;
+  padding: 1.6rem 5.6rem 3.3rem; 
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
 
   .back {
-    margin-left: 19rem;
-    margin-top: 4rem;
-  }
-  @media (max-width: ${DEVICE_BREAKPOINT.MD}) {
-    .back {
-      margin-left: 10rem;            
+    font-size: 1.6rem;
+
+    svg {
+      font-size: 2.2rem;
     }
   }
+
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+    width: 100%;
+    height: 100%;
+
+    .back {
+    margin-top: 4rem;
+    font-size: 2.4rem;
+   }
+  }  
 `
 
 export const Form = styled.form`
-  max-width: 112rem;
-  width: 70%;
-  margin: 0 auto 11rem;
+  margin-top: 2.4rem; 
+  margin-bottom: 11rem;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  
+  gap: 2.4rem;
+
   h2 {
     align-self: start;
-    margin-bottom: 3.2rem;
-    margin-top: 2.4rem;
-
-    font-size: 3.2rem;
+    font-size: 2.8rem;
     font-weight: 500;
     color: ${({theme}) => theme.COLORS.LIGHT_300}
   }
@@ -54,11 +63,10 @@ export const Form = styled.form`
   > .infos {
     width: 100%;
     display: flex;
-    flex-direction: row;
-    align-items: center;    
-    gap: 3.2rem;
-    margin-bottom: 3.2rem;
-
+    flex-direction: column;
+    align-items: start;    
+    gap: 2.4rem;
+    
     #label {
       display: flex;
       width: 100%;
@@ -84,12 +92,15 @@ export const Form = styled.form`
           color: ${({theme}) => theme.COLORS.LIGHT_400};
         }
 
-      > Input {
+      > .edit {
         background-color: ${({theme}) => theme.COLORS.DARK_800};
+        width: 100%;
+        border: none;
+        outline: none;
       }     
            
       #categoria {
-        width: 24rem;
+        width: 100%;
         height: 4.8rem;
         padding: 1.6rem;
         border: none;
@@ -100,7 +111,9 @@ export const Form = styled.form`
       }
       
       #price {
-        width: 24rem;
+        width: 100%;
+        border: none;
+        outline: none;
       }      
     }
   }
@@ -143,48 +156,73 @@ export const Form = styled.form`
 
     }
   }
+   
+  .btn-save {
+     width: 100%;
+     height: 4.8rem;
+     padding: 1.2rem 2rem;
+     margin-top: 3.2rem;
+     border-radius: 0.5rem;
+     border: none;
+  
+     background-color: ${({theme}) => theme.COLORS.TOMATO_400};
+  
+     text-align: center;
+     font-size: 1.4rem;
+     font-weight: 500;
+     color: ${({theme}) => theme.COLORS.LIGHT_100};
+  }    
+  
+
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 0;
+
+    > h2 {
+      margin-bottom: 3.2rem;
+      margin-top: 2.4rem;
+      font-size: 3.2rem;
+    }
+
+    > .infos {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;    
+    gap: 3.2rem;
+    margin-bottom: 3.2rem;
+   }
+   #categoria {
+      width: 24rem;
+    }
+    
+   #price {
+    width: 24rem;
+  } 
+
   .btn-save {
     width: 17rem;
     height: 4.8rem;
     padding: 1.2rem 2rem;
     margin-top: 3.2rem;
+    border-radius: 0.5rem;
 
     display: flex;
     align-self: flex-end;
     justify-content: center;
     align-items: center;
-    
-
-    border-radius: 0.5rem;
-    border: none;
-
-    background-color: ${({theme}) => theme.COLORS.TOMATO_400};
-
-    text-align: center;
-    font-size: 1.4rem;
-    font-weight: 500;
-    color: ${({theme}) => theme.COLORS.LIGHT_100};
-    
   }
-  @media (max-width: ${DEVICE_BREAKPOINT.MD}) {
-    width: 100%;
-    margin: auto; 
-    align-items: center;
-
-    .infos {
-      flex-direction: column;
-      align-items: normal;
-    }
-    #categoria {
-        width: 100%;
-    }
-  }
+}
 `
 
 export const InputFile = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 1.6rem;  
+  gap: 1.6rem; 
+  width: 100%;
 
   :first-child {
     font-family: ${({theme}) => theme.FONTS.Secondary};
@@ -192,7 +230,7 @@ export const InputFile = styled.div`
   }
 
   :nth-child(2) {
-    width: 24rem;
+    width: 100%;
     height: 4.8rem;
     padding: 1.2rem 3.2rem;
     border-radius: 0.8rem;
@@ -203,7 +241,7 @@ export const InputFile = styled.div`
     background-color: ${({theme}) => theme.COLORS.DARK_800};
 
     color: ${({theme}) => theme.COLORS.LIGHT_100} ;
-    text-align: center;
+    text-align: start;
     font-size: 1.4rem;
     font-weight: 500;
 
@@ -212,21 +250,20 @@ export const InputFile = styled.div`
       height: 2.4rem;
       margin-right: 0.8rem;      
     }
-
     #image {
       display: none;
     }
+  }  
+
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+    :nth-child(2) {
+     text-align: center;
+    }
   }
-@media (max-width: ${DEVICE_BREAKPOINT.MD}) {
-  
-  :nth-child(2) {
-    width: 100%;
-  }
-}    
-`
+` 
 
 export const Ingredients = styled.div`
-  width: 85rem;
+  width: 100%;
   height: 4.8rem;
   padding: 0.4rem 0.8rem;
   border-radius: 0.8rem;
@@ -236,6 +273,10 @@ export const Ingredients = styled.div`
   justify-content: start;
   gap: 1.6rem;
 
-  background-color: ${({theme}) => theme.COLORS.DARK_800};  
+  background-color: ${({theme}) => theme.COLORS.DARK_800}; 
+  
+  @media (min-width: ${DEVICE_BREAKPOINT.LG}) {
+    width: 85rem;
+  }
+  
 `
-
