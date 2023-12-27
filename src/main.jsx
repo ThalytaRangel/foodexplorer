2326a0949c0c6b7b13d779252ac977e13388602b
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-27 12:42:02 -0300

    refactoring/removing the app file and using main.js to start the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import GlobalStyle from "./styles/global";
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/theme/default";
+
+import { AuthProvider } from "./hooks/auth";
+import { Routes } from "./routes";
 import { register } from "swiper/element/bundle";
 
 register();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+    </ThemeProvider>
   </React.StrictMode>,
 );
