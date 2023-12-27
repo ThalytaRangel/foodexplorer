aa8ae754c538de3341b7ed59fedd0a42374c04a8
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-26 21:24:58 -0300

    feat/TASK-0057: added favorites route

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -4,6 +4,7 @@ import { Home } from "../pages/Home";
 import { EditDish } from "../pages/EditDish";
 import { DishDetails } from "../pages/DishDetails";
 import { NewDish } from "../pages/NewDish";
+import { Favorites } from "../pages/Favorites";
 
 export function AppRoutes() {
   return (
@@ -12,6 +13,7 @@ export function AppRoutes() {
       <Route path="/new" element={<NewDish />} />
       <Route path="/edit/:id" element={<EditDish />} />
       <Route path="/details/:id" element={<DishDetails />} />
+      <Route path="/favorites" element={<Favorites />} />
     </Routes>
   );
 }
