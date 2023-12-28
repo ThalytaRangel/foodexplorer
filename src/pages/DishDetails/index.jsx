24fcdcdb2cb514f4283c9cd73f288028d5ec3f56
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-28 18:10:05 -0300

    feat/TASK-0063: added dish.data from api

--- src/pages/DishDetails/index.jsx
+++ src/pages/DishDetails/index.jsx
@@ -1,41 +1,61 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { Container, Content, DishIngredients } from "./styles";
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
-import DishImg from "../../assets/salada.png";
+import dishPlaceholder from "../../assets/dishPlaceholder.png";
 import { ButtonTxt } from "../../components/ButtonTxt";
 import { AddToCart } from "../../components/AddToCart";
 import { Ingredient } from "../../components/Ingredient";
 import { IoIosArrowBack } from "react-icons/io";
 import { Button } from "../../components/Button";
 
+import { api } from "../../services/api";
+
 export function DishDetails() {
+  const [dish, setDish] = useState(null);
+  const params = useParams();
+
+  const dishImgURL = dish?.image
+    ? `${api.defaults.baseURL}/files/${dish.image}`
+    : dishPlaceholder;
+
+  useEffect(() => {
+    async function fetchDishes() {
+      const response = await api.get(`/dishes/${params.id}`);
+      setDish(response.data);
+    }
+    fetchDishes();
+  }, [params.id]);
+
   return (
     <Container>
       <Header />
       <Content>
         <ButtonTxt title="voltar" icon={IoIosArrowBack} />
-        <main>
-          <img src={DishImg} alt="Foto da Salada Ravanello" />
-          <div id="info">
-            <h2>Salada Ravanello</h2>
-            <p>
-              Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.
-              O pão naan dá um toque especial.
-            </p>
-            <DishIngredients>
-              <Ingredient title="alface" />
-              <Ingredient title="cebola" />
-              <Ingredient title="pão naan" />
-              <Ingredient title="pepino" />
-              <Ingredient title="rabanete" />
-              <Ingredient title="tomate" />
-            </DishIngredients>
-            <div id="cartSelector">
-              <AddToCart />
-              <Button title="incluir ∙ R$ 25,00" />
+        {dish && (
+          <main>
+            <img src={dishImgURL} alt="Foto da Salada Ravanello" />
+            <div id="info">
+              <h2>{dish.name}</h2>
+              <p>{dish.description}</p>
+              {dish.ingredients && (
+                <DishIngredients>
+                  {dish.ingredients.map(ingredient => (
+                    <Ingredient
+                      key={String(ingredient.id)}
+                      title={ingredient.name}
+                    />
+                  ))}
+                </DishIngredients>
+              )}
+              <div id="cartSelector">
+                <AddToCart />
+                <Button title={`incluir ∙ ${dish.price}`} />
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        )}
       </Content>
       <Footer />
     </Container>
