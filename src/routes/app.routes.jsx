23f28f935a688ebbe791cd7a696eaecce3bc4bfa
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-29 20:41:36 -0300

    feat/TASK-0059: adding condition user.admin in app.routes

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 
 import { Home } from "../pages/Home";
 import { EditDish } from "../pages/EditDish";
@@ -6,14 +6,27 @@ import { DishDetails } from "../pages/DishDetails";
 import { NewDish } from "../pages/NewDish";
 import { Favorites } from "../pages/Favorites";
 
+import { useAuth } from "../hooks/auth";
+
 export function AppRoutes() {
+  const { user } = useAuth();
+
+  const { isAdmin } = Boolean(user.admin);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewDish />} />
-      <Route path="/edit/:id" element={<EditDish />} />
-      <Route path="/details/:id" element={<DishDetails />} />
       <Route path="/favorites" element={<Favorites />} />
+      <Route path="/details/:id" element={<DishDetails />} />
+
+      {isAdmin && (
+        <>
+          <Route path="/edit/:id" element={<EditDish />} />
+          <Route path="/new" element={<NewDish />} />
+        </>
+      )}
+
+      <Route path="*" element={<Navigate to={"/"} replace />} />
     </Routes>
   );
 }
