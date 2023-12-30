7a3849c6397dc75211d4b77b3a30598deea27b73
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2023-12-30 11:11:33 -0300

    feat/TASK-0059: adding admin.routes and condition to switch between routes

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -1,15 +1,19 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 
 import { Home } from "../pages/Home";
+import { EditDish } from "../pages/EditDish";
 import { DishDetails } from "../pages/DishDetails";
+import { NewDish } from "../pages/NewDish";
 import { Favorites } from "../pages/Favorites";
 
-export function AppRoutes() {
+export function AdminRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/favorites" element={<Favorites />} />
       <Route path="/details/:id" element={<DishDetails />} />
+      <Route path="/edit/:id" element={<EditDish />} />
+      <Route path="/new" element={<NewDish />} />
 
       <Route path="*" element={<Navigate to={"/"} replace />} />
     </Routes>
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,21 @@ import { BrowserRouter } from "react-router-dom";
 import { useAuth } from "../hooks/auth";
 import { AppRoutes } from "./app.routes";
 import { AuthRoutes } from "./auth.routes";
+import { AdminRoutes } from "./admin.routes";
 
 export function Routes() {
   const { user } = useAuth();
+  const isAdmin = Boolean(user.admin);
 
-  return <BrowserRouter>{user ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>;
+  function AccessRoute() {
+    if (isAdmin) {
+      return <AdminRoutes />;
+    } else {
+      return <AppRoutes />;
+    }
+  }
+
+  return (
+    <BrowserRouter>{user ? <AccessRoute /> : <AuthRoutes />}</BrowserRouter>
+  );
 }
