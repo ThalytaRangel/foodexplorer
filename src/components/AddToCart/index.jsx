f56c3771205f93220bed5798fa3e7b9a3b846f56
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-01-10 17:22:59 -0300

    feat/FEAT:0005: added functions increase and decrease in AddtoCart component

--- src/components/AddToCart/index.jsx
+++ src/components/AddToCart/index.jsx
@@ -1,14 +1,31 @@
 import { Container } from "./styles";
+import { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
 export function AddToCart() {
+  const [quantityOfItems, setQuantityOfItems] = useState("01");
+
+  function increase() {
+    if (Number(quantityOfItems) < 9) {
+      let newMinutes = Number(quantityOfItems) + 1;
+      setQuantityOfItems(`0${newMinutes}`);
+    }
+  }
+
+  function decrease() {
+    if (Number(quantityOfItems) > 1) {
+      let newMinutes = Number(quantityOfItems) - 1;
+      setQuantityOfItems(`0${newMinutes}`);
+    }
+  }
+
   return (
     <Container>
-      <button id="minus">
+      <button id="minus" onClick={decrease}>
         <AiOutlineMinus />
       </button>
-      <span>01</span>
-      <button id="plus">
+      <span className="quantity">{quantityOfItems}</span>
+      <button id="plus" onClick={increase}>
         <AiOutlinePlus />
       </button>
     </Container>
