a7bbe7e457d757b420164fc23238f4938e99f15d
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-01-12 23:15:19 -0300

    fix/solve ingredients onclick issues

--- src/components/NewTag/index.jsx
+++ src/components/NewTag/index.jsx
@@ -1,11 +1,13 @@
 import { Container } from "./styles";
 import { FiPlus, FiX } from "react-icons/fi";
 
-export function NewTag({ isNew, ...rest }) {
+export function NewTag({ isNew, value, onClick, ...rest }) {
   return (
     <Container isNew={isNew}>
-      <input type="text" readOnly={!isNew} {...rest} />
-      <button type="button">{isNew ? <FiPlus /> : <FiX />}</button>
+      <input type="text" value={value} readOnly={!isNew} {...rest} />
+      <button type="button" onClick={onClick}>
+        {isNew ? <FiPlus /> : <FiX />}
+      </button>
     </Container>
   );
 }
