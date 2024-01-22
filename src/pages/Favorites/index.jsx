c137ab6c2fa0836b2d67bd1fb42370cc57dd3ad2
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-01-22 10:49:41 -0300

    refactor/removed unused imports

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import { Container, Content, Card } from "./styles";
 import { Header } from "../../components/Header";
