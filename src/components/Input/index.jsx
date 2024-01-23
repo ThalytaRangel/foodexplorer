3c9b34e43fb26d274fe92e9c238d3f7147a79a53
Thalyta Rangel <thalyta_ornelas@hotmail.com>
2024-01-23 15:36:34 -0300

    fix/solve placeholder issues

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,6 @@
 import { Container } from "./styles";
 
-export function Input({
-  type = "text",
-  placeholder = "text",
-  value = "text",
-  ...rest
-}) {
+export function Input({ type, placeholder, value, ...rest }) {
   return (
     <Container {...rest}>
       <input type={type} placeholder={placeholder} value={value} />
